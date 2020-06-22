eb6957c6cbb1409001751c6efba7368a8618fbdd
Guntram Blohm <gbl@guntram.de>
2020-06-22 13:08:12 +0200

    Give the brewing stand a "take all" button and a percentage display

--- src/main/java/de/guntram/mcmod/easiercrafting/ExtendedGuiBrewingStand.java
+++ src/main/java/de/guntram/mcmod/easiercrafting/ExtendedGuiBrewingStand.java
@@ -1,11 +1,13 @@
 package de.guntram.mcmod.easiercrafting;
 
 import net.minecraft.client.gui.screen.ingame.BrewingStandScreen;
+import net.minecraft.client.gui.widget.AbstractButtonWidget;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.screen.BrewingStandScreenHandler;
 import net.minecraft.screen.slot.SlotActionType;
 import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
 import org.lwjgl.glfw.GLFW;
 
 public class ExtendedGuiBrewingStand extends BrewingStandScreen implements SlotClickAccepter {
@@ -19,6 +21,15 @@ public class ExtendedGuiBrewingStand extends BrewingStandScreen implements SlotC
     @Override
     protected void init() {
         super.init();
+        this.addButton(new AbstractButtonWidget(x+130, y+50, 40, 20, new TranslatableText("easiercrafting.brewing.takeall")) {
+            @Override
+            public void onClick(double x, double y) {
+                // for some reason, this order seems to work better than 0 1 2
+                slotClick(1, 0, SlotActionType.QUICK_MOVE);
+                slotClick(2, 0, SlotActionType.QUICK_MOVE);
+                slotClick(0, 0, SlotActionType.QUICK_MOVE);
+            }
+        });
         this.recipeBook.afterInitGui();
     }
 
---
+++ src/main/java/de/guntram/mcmod/easiercrafting/mixins/HandledScreenMixin.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.guntram.mcmod.easiercrafting.mixins;
+
+import de.guntram.mcmod.easiercrafting.accessorInterfaces.PropertyDelegateProvider;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.ingame.AbstractFurnaceScreen;
+import net.minecraft.client.gui.screen.ingame.BrewingStandScreen;
+import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.client.resource.language.I18n;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.screen.BrewingStandScreenHandler;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.StringRenderable;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+/**
+ *
+ * @author gbl
+ */
+@Mixin(HandledScreen.class)
+public abstract class HandledScreenMixin extends Screen {
+    
+    @Shadow protected int backgroundWidth = 176;
+    @Shadow protected int backgroundHeight = 166;
+    @Shadow protected ScreenHandler handler;
+    @Shadow @Final protected PlayerInventory playerInventory;
+
+    public HandledScreenMixin(ScreenHandler handler, PlayerInventory playerInventory_1, Text title) {
+        super(title);
+    }
+    
+    @Inject(method="drawForeground", at=@At("HEAD"), cancellable = true)
+    private void patchTitleWithBurntime(MatrixStack stack, int x, int y, CallbackInfo ci) {
+        Object thisCopy=this;
+        if ((thisCopy instanceof AbstractFurnaceScreen)) {
+            PropertyDelegateProvider pde = (PropertyDelegateProvider) handler;
+            int itemsLeft=0, fuelLeftPercent=0, itemDonePercent=0;
+            if (pde.getPropertyDelegate(3) != 0) {
+                itemsLeft = pde.getPropertyDelegate(0)/pde.getPropertyDelegate(3);
+                itemDonePercent = pde.getPropertyDelegate(2)*100/pde.getPropertyDelegate(3);
+            }
+            if (pde.getPropertyDelegate(1)!=0) {
+                fuelLeftPercent = pde.getPropertyDelegate(0)*100/pde.getPropertyDelegate(1);
+            }
+
+            String titleText = this.title.getString() + " (" + itemsLeft + " " + I18n.translate("easiercrafting.furnace.itemsleft")+")";
+            this.textRenderer.draw(stack, titleText, (float)(this.backgroundWidth / 2 - this.textRenderer.getWidth(titleText) / 2), 6.0F, 4210752);
+            this.textRenderer.draw(stack, this.playerInventory.getDisplayName(), 8.0F, (float)(this.backgroundHeight - 96 + 2), 4210752);
+            this.textRenderer.draw(stack, itemDonePercent+" %", 20, 22, 4210752);
+            this.textRenderer.draw(stack, fuelLeftPercent+" %", 20, 58, 4210752);
+
+            //for (int i=0; i<4; i++) {
+            //    this.font.draw(""+((PropertyDelegateProvider)container).getPropertyDelegate(i), 5, 10+i*10, 0x000000);
+            //}
+
+            ci.cancel();
+        } else if (thisCopy instanceof BrewingStandScreen) {
+            int brewTime = ((BrewingStandScreenHandler)handler).getBrewTime();
+            StringRenderable titleText;
+            if (brewTime > 0) {
+                int brewPercent = 100-(brewTime*100/400);
+                titleText = new LiteralText(this.title.getString() + " ( " + brewPercent + "% )");
+            } else {
+                titleText = this.title;
+            }
+            this.textRenderer.draw(stack, titleText, (float)(this.backgroundWidth / 2 - this.textRenderer.getWidth(titleText) / 2), 6.0F, 4210752);
+            this.textRenderer.draw(stack, this.playerInventory.getDisplayName(), 8.0F, (float)(this.backgroundHeight - 96 + 2), 4210752);
+            ci.cancel();
+        }
+    }
+}
